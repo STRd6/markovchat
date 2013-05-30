8bf34c81a849080970800013479343d4dcfbe693
Daniel Moore <yahivin@gmail.com>
2013-05-30 13:39:53 -0700

    Dropping cached hash

--- markovchat.rb
+++ markovchat.rb
@@ -1,31 +1,29 @@
 require 'pp'
-require 'cached_hash'
 
 class MarkovChat
-
   attr_accessor :dbfile
 
   def initialize(dbfile="markov.db")
     @dbfile = dbfile
-    @nextwords = CachedHash.new
+    @nextwords = Hash.new
     @nextwords_total = {}
   end
-  
+
   def database
     @nextwords
   end
-  
+
   def nw
     @nextwords
   end
 
   def random_pair
-    @nextwords.random_key
+    @nextwords.keys.sample
   end
-  
+
   def pair_starting_with(word)
     word = word.to_sym
-    @nextwords.find_random_key{|k| k.first == word}
+    @nextwords.keys.select {|k| k.first == word}.sample
   end
 
   def random_start
@@ -39,10 +37,10 @@ class MarkovChat
   def chat(*args)
     args = pair_starting_with(args.first) if args.size == 1
 
-    while args.size != 2 or args.any?{|arg| arg.nil?} 
+    while args.size != 2 or args.any?{|arg| arg.nil?}
       args = random_start
     end
-    
+
     w1, w2    = args.map{|w| w.to_sym}
     sentence  = [w1, w2]
 
@@ -50,7 +48,7 @@ class MarkovChat
       sentence << nw
       w1,w2 = w2,nw
     end
-    
+
     sentence.join(" ")
   end
 
@@ -58,11 +56,11 @@ class MarkovChat
     @nextwords_total[key] ||= 0
   end
 
-  def inc_total(key)  
+  def inc_total(key)
     @nextwords_total[key] ||= 0
     @nextwords_total[key] += 1
   end
-  
+
   def nextwords(key)
     @nextwords[key] ||= {}
   end
@@ -74,7 +72,7 @@ class MarkovChat
     thresh  = rand(total(key))
     total   = 0
     nw      = nil
-    
+
     for word, num in nexts
       total += num
       if total > thresh
@@ -82,17 +80,17 @@ class MarkovChat
         break
       end
     end
-    
+
     nw
   end
-  
+
   def add_sentence(sentence)
     ws = [nil] + sentence.split.map{|w| w.to_sym} + [nil]
     for i in (0...ws.size-2)
       add_triple( *ws[i..i+2] )
     end
   end
-  
+
   def add_triple(w1, w2, w3)
     key = [w1,w2]
     nexts = nextwords(key)
@@ -100,7 +98,7 @@ class MarkovChat
     nexts[w3] += 1
     inc_total(key)
   end
-  
+
   def dump
     puts "-"*50
     puts "Contents of markov database:"
@@ -112,61 +110,56 @@ class MarkovChat
   def tempfile
     "#{dbfile}.temp"
   end
-  
+
   def locked?
     File.exists?(tempfile)
   end
-  
+
   def save
     if locked?
       puts "+ Error! Can't save because #{tempfile.inspect} already exists."
       puts "  (Either we're already saving in the background, or you crashed before and"
       puts "   you should delete that file.)"
-      
+
       false
     else
       puts "+ Writing #{tempfile}..."
       open(tempfile, "wb") do |f|
         f.write Marshal.dump([@nextwords, @nextwords_total])
       end
-      
+
       if File.exists?(dbfile)
         puts "  |_ backing up #{dbfile} (to .bak)..."
         File.rename(dbfile, "#{dbfile}.bak")
       end
-      
+
       puts "  |_ moving #{tempfile} into place"
       File.rename(tempfile, dbfile)
-      
+
       puts "  |_ Done!"
       puts
 
       true
     end
   end
-  
+
   #
-  # Save the database in the background (by forking) 
+  # Save the database in the background (by forking)
   #
   def background_save
     Process.detach( fork { save } )
   end
-  
+
   def load
     puts "Loading #{dbfile}..."
     open(dbfile) do |f|
       @nextwords, @nextwords_total = Marshal.load(f.read)
     end
-    
-    unless @nextwords.is_a? CachedHash
-      puts "*** Upgrading Hash to a CachedHash! ***"
-      @nextwords = @nextwords.to_cached_hash
-    end
-    
+
     puts "  |_ Done! #{@nextwords.size} pairs..."
     puts
   end
-  
+
 end
 
 
@@ -174,7 +167,7 @@ end
 if $0 == __FILE__
 
   m = MarkovChat.new
-  
+
   m.add_sentence("hi there how are you")
   m.add_sentence("how is trix doing")
   m.add_sentence("hey dude what's up")
@@ -182,27 +175,27 @@ if $0 == __FILE__
   m.add_sentence("hi there you are groovy")
   m.add_sentence("hi there you are amazing")
   m.add_sentence("hi there you are a manly man")
-  
+
   m.save
-  
+
   m.background_save
-  
+
   #puts "hit enter to continue..."; gets
-  
+
   n = MarkovChat.new
   while m.locked?; sleep 0.1; end
   puts "lock released!"
   n.load
   n.dump
-  
+
   p [:pair_starting_with, n.pair_starting_with("hi")]
   p [:pair_starting_with, n.pair_starting_with("hi")]
-  
+
   p n.chat("hi", "there")
-  
-  
+
+
   5.times do
     p n.chat
   end
-  
+
 end
